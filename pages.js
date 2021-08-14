e694ecc57fb84db301e6562993ab218c0dd6c979
tanyjnaaman <67091469+tanyjnaaman@users.noreply.github.com>
2021-08-14 16:00:08 +0800

    Fixing small typos

--- pages.js
+++ pages.js
@@ -5,7 +5,7 @@ const repoURL = "https://github.com/tanyjnaaman/tanyjnaaman.github.io.git";
 ghpages.publish(
   pathname,
   {
-    branch: "master",
+    branch: "main",
     repo: repoURL,
   },
   (err) => {
